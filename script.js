c2bed5096c68ee781ccd647eeb02a47665fda3ff
RA-2005 <your-email@example.comramloganathan2005@gmail.com>
2025-08-10 08:43:07 +0530

    Added new change -Html,css,JS

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("form-message").innerText = "Thank you! I’ll get back to you soon.";
-  this.reset();
-});
